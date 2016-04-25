89d6d64e92b252142218569a55965d3f6612baea
XJR <1270508005@qq.con>
2016-04-25 16:03:40 +0800

    创建了一个Person类

---
+++ weChat/weChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weChat
+//
+//  Created by 邢京荣 on 16/4/25.
+//  Copyright © 2016年 邢京荣. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weChat/weChat/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weChat
+//
+//  Created by 邢京荣 on 16/4/25.
+//  Copyright © 2016年 邢京荣. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
