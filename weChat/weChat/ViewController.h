d79fafb590edc18042872446f6286e75b71b85c6
XJR <1270508005@qq.con>
2016-04-25 16:01:06 +0800

    初始化项目

---
+++ weChat/weChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weChat
+//
+//  Created by 邢京荣 on 16/4/25.
+//  Copyright © 2016年 邢京荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weChat/weChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weChat
+//
+//  Created by 邢京荣 on 16/4/25.
+//  Copyright © 2016年 邢京荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
